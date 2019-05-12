ec607e28b752aff124494c65fc25d3cb482c01ba
xwp <xwp_fullstack@163.com>
2019-05-12 17:07:53 +0800

    update_code

--- bo_test009.py
+++ bo_test009.py
@@ -1,4 +1,17 @@
 #! /usr/bin/python3.5
 
-list_1 = [i for i in range(10)]
-print(list_1)
+
+def pro_c(func):
+    def fn_in(*args,**kwargs):
+        list_1 = [i for i in range(10)]
+        print(list_1)
+        func(*args,**kwargs)
+    return fn_in
+
+@pro_c
+def my_test():
+    print('my_git@github.com:create6/test02.git')
+    print('my_decorate')
+
+my_test()
+
